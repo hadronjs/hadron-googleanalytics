02cfe804e756d5344f160016fc49e9fec0b05727
Mario Casciaro <mariocasciaro@gmail.com>
2014-01-07 20:03:18 +0000

    Use Hadron 0.2 new plugin hooks

--- assets/js/plugin-googleanalytics.js
+++
@@ -1,8 +0,0 @@
-
-(function(shatter) {
-  shatter.use('saveSettingsCtrl', 'controllerImpl', function($scope) {
-    $scope.saveGA = function(settings, invalid) {
-      return $scope.save('googleAnalyticsCode', settings, invalid);
-    };
-  });
-})(window.shatter);
